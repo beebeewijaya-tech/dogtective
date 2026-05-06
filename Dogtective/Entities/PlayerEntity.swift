c89692e4514cdcb0dfb55075036a312a36a1ca18
Bee Bee Wijaya <79556627+beebeewijaya-tech@users.noreply.github.com>
2026-05-06 13:36:03 +0700

    feat: implementing player idle state with animation (#2)
    -- What
    
    implementing player idle rendering the gamescene on didMove() lifecycle
    
    the goal is to show player avatar when idle.
    
    implementation is on the entities for the player configuration regarding the texture and animation function ( accepting frames and can reusable )
    
    also the PlayerSystem will be doing the rendering logic, movement logic ( afterwards ) and logic with player
    
    -- How
    
    by using ECS architecture and atlas for assets, and mostly using SKActions for the animate
    
    putting player entity to inherit from SKSpriteNode and provide several value into that and giving the idle state and animation frame

--- Dogtective/Entities/PlayerEntity.swift
+++ Dogtective/Entities/PlayerEntity.swift
@@ -7,7 +7,58 @@
 
 import SpriteKit
 
+enum PlayerState {
+    case idle, moving
+}
+
+struct PlayerInfo {
+    var texture: SKTexture?
+    var coordinates: CGPoint?
+    var state: PlayerState?
+}
+
 
 class PlayerEntity: SKSpriteNode {
-    // TODO: implement player entity, add bunch of property and items
+    var playerInfo: PlayerInfo?
+    var playerIdleAtlas = SKTextureAtlas(named: "player_idle")
+    var playerIdleFrames: [SKTexture] = []
+    
+    
+    init(playerInfo: PlayerInfo?) {
+        self.playerInfo = playerInfo
+        let firstFrame = playerIdleAtlas.textureNamed("mrbones-idle_00") // for first render or default
+        
+        super.init(
+            texture: playerInfo?.texture ?? firstFrame,
+            color: .clear,
+            size: .zero
+        )
+        
+        prepareIdleFrames()
+    }
+    
+    func prepareIdleFrames() {
+        // player idle injecting preparation frame
+        
+        for i in 1...62 {
+            playerIdleFrames.append(playerIdleAtlas.textureNamed(String(format: "mrbones-idle_%02d", i)))
+        }
+    }
+    
+    func idlePlayerState() {
+        // run animation after frames injected
+        playerInfo?.state = .idle
+        animatePlayer(frames: playerIdleFrames)
+    }
+    
+    func animatePlayer(frames: [SKTexture]) {
+        // animatePlayer will run animation given the frames
+        
+        let animate = SKAction.animate(with: frames, timePerFrame: 0.01)
+        self.run(SKAction.repeatForever(animate))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
 }
---
+++ Dogtective/Systems/PlayerSystem.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerSystem.swift
+//  Dogtective
+//
+//  Created by Bee Wijaya on 05/05/26.
+//
+
+import SpriteKit
+
+// MARK: - Player System
+// business logic regarding player, location, animation
+extension GameScene {
+    func setupPlayer() {
+        guard let entity = playerEntity else { return }
+        
+        let width = entity.playerIdleFrames.first?.size().width ?? 0
+        let height = entity.playerIdleFrames.first?.size().height ?? 0
+
+        entity.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        entity.size = CGSize(width: width, height: height)
+        addChild(entity)
+        
+        entity.idlePlayerState()
+    }
+}
