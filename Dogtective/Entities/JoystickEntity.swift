acc810c881a61ec0914c736aaa5da8ba8d13e789
Bee Wijaya <79556627+beebeewijaya-tech@users.noreply.github.com>
2026-05-13 13:34:52 +0700

    refactor: update JOYSTICK into ECS structure (#19)
    -- What
    
    we want to update joystick to have ECS standard to be Entity, Component, System
    
    -- How
    
    - Putting the JoyStick entity as only to do the UI for rendering image and positioning to put at bottom left
    - JoyStick clamp and release behavior will be lives within component
    - JoyStick touch event, release event, setup the position will be lives within the system

--- Dogtective/Entities/JoystickEntity.swift
+++ Dogtective/Entities/JoystickEntity.swift
@@ -5,42 +5,52 @@
 //  Created by Bee Wijaya on 06/05/26.
 //
 
-import SpriteKit
+import GameplayKit
 
-class JoystickEntity: SKNode {
+class JoystickEntity: BaseEntity {
     let base: SKSpriteNode
     let knob: SKSpriteNode
+    let sceneSize: CGSize
     var maxRadius: CGFloat = 0
-    var joystickUI: UITouch!
-    var sceneSize: CGSize
-
+    var joystickUI: UITouch?
+    
     init(sceneSize: CGSize) {
+        self.sceneSize = sceneSize
         self.base = SKSpriteNode(imageNamed: "joystick_base")
         self.knob = SKSpriteNode(imageNamed: "joystick_knob")
-        self.sceneSize = sceneSize
+
+        let node = SKNode()
+        node.addChild(self.base)
+        node.addChild(self.knob)
         
-        super.init()
-        
+        super.init(node: node)
         self.setupBase()
         self.setupKnob()
         self.setupRadius()
+        
+        // MARK: - Prepare component
+        addComponent(JoystickComponent(maxRadius: maxRadius))
+    }
+    
+    // MARK: - Getter func for not always casting
+    var joystickComponent: JoystickComponent? {
+        component(ofType: JoystickComponent.self)
     }
     
     
+    // MARK: - preparing the joystick UI
     private func setupBase() {
         base.size = CGSize(width: 120, height: 120)
         base.position = CGPoint(x: (-sceneSize.width / 2) + 120, y: (-sceneSize.height / 2) + 100)
-        addChild(base)
     }
-    
+
     private func setupKnob() {
         knob.size = CGSize(width: 70, height: 70)
         knob.position = CGPoint(x: (-sceneSize.width / 2) + 120, y: (-sceneSize.height / 2) + 100)
-        addChild(knob)
     }
-    
+
     private func setupRadius() {
-        maxRadius = base.size.width / 2
+        self.maxRadius = base.size.width / 2
     }
     
     required init?(coder aDecoder: NSCoder) {
