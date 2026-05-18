d9607a4d5ee8f538fed9f5ecb27ec84aca352126
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-05-18 21:28:19 +0700

    feat: implementing cutscene for the evidence
    -- What
    
    Evidence number 7 will lead to cutscene number 3, and Final evidence which is the fence will lead to cutscene 4
    Also implementing the save state of the evidence, we want to store which evidence has been

--- Dogtective/ViewModel/CutsceneViewModel.swift
+++ Dogtective/ViewModel/CutsceneViewModel.swift
@@ -9,9 +9,16 @@ import Combine
 
 
 class CutsceneViewModel: ObservableObject {
+    @Published var cutscene: Int = 0
     @Published var cutsceneName: String = ""
     
-    func setCutscene(cutsceneName: String) {
-        self.cutsceneName = cutsceneName
+    func setCutscene(cutscene: Int) {
+        self.cutscene = cutscene
+        self.cutsceneName = "cutscene_\(cutscene)"
+    }
+    
+    func incrementCutscene() {
+        self.cutscene += 1
+        self.cutsceneName = "cutscene_\(self.cutscene)"
     }
 }
