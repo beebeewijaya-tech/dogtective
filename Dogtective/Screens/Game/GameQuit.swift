16038afd3bce09efb8c521a59f8a1cd4a0895a7b
Michael David Sin <146686816+michaeldavidsinn@users.noreply.github.com>
2026-05-18 15:59:46 +0700

    feat : create pause and quit modal

---
+++ Dogtective/Screens/Game/GameQuit.swift
@@ -0,0 +1,56 @@
+//
+//  GameQuit.swift
+//  Dogtective
+//
+//  Created by Michael David Sin on 18/05/26.
+//
+
+import SwiftUI
+
+struct GameQuit: View {
+
+    @Binding var showQuitConfirmation: Bool
+    @Binding var isPaused: Bool
+    @EnvironmentObject var pageStateViewModel: PageStateViewModel
+    
+    var body: some View {
+        ZStack {
+            Image("quit_popup")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 420, height: 240)
+                .shadow(color: .black.opacity(0.4), radius: 10, x: 0, y: 8)
+            
+            HStack(spacing: -5) {
+                
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        showQuitConfirmation = false
+                        isPaused = false
+                    }
+                }) {
+                    Image("quit_no")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 160, height: 45)
+                }
+                
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        showQuitConfirmation = false
+                        isPaused = false
+                        pageStateViewModel.state = .level
+                    }
+                }) {
+                    Image("quit_yes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 160, height: 45)
+                }
+                
+            }
+            .offset(y: 35)
+        }
+        .transition(.scale(scale: 0.9).combined(with: .opacity))
+    }
+}
