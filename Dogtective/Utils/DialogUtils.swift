f4011f85dab36e69e8bc8d360885bdbb2cbe79e2
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-05-19 16:34:43 +0700

    feat: npc placement to the map system
    -- What
    
    we update npc placement position and the dialog for chichi and billy

--- Dogtective/Utils/DialogUtils.swift
+++ Dogtective/Utils/DialogUtils.swift
@@ -23,4 +23,30 @@ struct DialogUtils {
             Dialog(message: "Stay safe out there.", evidence: false),
         ]
     }
+    
+    static func billyDialogs() -> [Dialog] {
+        return [
+            Dialog(message: "I hope you soon find the culprit", evidence: false),
+            Dialog(message: "This corn taste so good", evidence: false),
+            Dialog(message: "Why that kid always playing there?", evidence: false),
+        ]
+    }
+    
+    static func chichiDialogs() -> [Dialog] {
+        return [
+            Dialog(message: "Hachoooo! I can't believe they suspect me", evidence: false),
+            Dialog(message: "You believe me right detective?", evidence: false),
+            Dialog(message: "I hope the truth will prevail", evidence: false),
+        ]
+    }
+    
+    static func billyNewDialog() -> [Dialog] {
+        return [
+            Dialog(message: "W-Why are you looking at me like that? I didn’t do anything!", evidence: false),
+            Dialog(message: "You’re wasting your time! Go question someone else already!", evidence: false),
+            Dialog(message: "Those clues don’t prove anything… right? They could belong to anyone!", evidence: false),
+            Dialog(message: "I only wanted to scare them a little… I didn’t think things would go this far…", evidence: false),
+            Dialog(message: "No… no, this can’t be happening. You’re not seriously blaming me, are you?", evidence: false),
+        ]
+    }
 }
