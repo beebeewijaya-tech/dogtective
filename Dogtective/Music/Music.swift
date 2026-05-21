baad8dce4905c3bc75158b389b47e5f68bf8e2e4
Michael David Sin <146686816+michaeldavidsinn@users.noreply.github.com>
2026-05-21 14:26:59 +0700

    feat: backpack sfx

--- Dogtective/Music/Music.swift
+++ Dogtective/Music/Music.swift
@@ -12,19 +12,20 @@ import AVFoundation
 class AudioManager: ObservableObject {
     static let shared = AudioManager()
     var player: AVAudioPlayer?
-
+    var sfxPlayer: AVAudioPlayer?
+    
     func playMusic(fileName: String) {
-        guard let url = Bundle.main.url(forResource: "dogtective_song", withExtension: "wav") else { return }
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "wav") else { return }
         
         do {
             player = try AVAudioPlayer(contentsOf: url)
-            player?.numberOfLoops = -1 // Loop indefinitely
+            player?.numberOfLoops = -1
             player?.play()
         } catch {
             print("Could not find or play the audio file.")
         }
     }
-
+    
     func stopMusic() {
         player?.stop()
     }
@@ -34,6 +35,18 @@ class AudioManager: ObservableObject {
             player?.play()
         } else {
             player?.pause()
+        }
+    }
+    
+    func playSFX(fileName: String, ext: String = "wav") {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ext) else { return }
+        
+        do {
+            sfxPlayer = try AVAudioPlayer(contentsOf: url)
+            sfxPlayer?.numberOfLoops = 0 
+            sfxPlayer?.play()
+        } catch {
+            print("Could not find or play the SFX file: \(fileName)")
         }
     }
 }
